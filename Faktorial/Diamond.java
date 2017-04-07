fefc1c3c90105967bc4590e60fe0fda3eb8eaa39
Indita Nurhafiza <inditanrhfz@gmail.com>
2017-04-07 10:37:46 +0700

    Update Diamond.java

--- Faktorial/Diamond.java
+++ Faktorial/Diamond.java
@@ -11,27 +11,27 @@ public class Diamond {
 		int l = 1;
 		for(int i=1;i<=angka;i++){
 		for(int k=angka;k>i;k--){
-		System.out.print(" ");
+		System.out.print("0");
 		}
 		for(int j=1;j<=l;j++){
 		System.out.print("+");
 		}
 		l+=2;
 		 
-		System.out.println("");
+		System.out.println("0");
 		}
 		l -= 4;
 		 
 		for(int i=(angka-1);i>=1;i--){
 		for(int k=i;k<=(angka-1);k++){
-		System.out.print(" ");
+		System.out.print("0");
 		}
 		for(int j=l;j>=1;j--){
 		System.out.print("+");
 		}
 		l-=2;
 		 
-		System.out.println("");
+		System.out.println("0");
 		}
  
 	}

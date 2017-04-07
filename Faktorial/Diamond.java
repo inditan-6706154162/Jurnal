1346726e31018838c45408a0d19596e33d3ba02d
Indita Nurhafiza <inditanrhfz@gmail.com>
2017-04-07 10:33:41 +0700

    Faktorial
    Diamond

---
+++ Faktorial/Diamond.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Diamond {
+
+    public static void main(String[] args) {
+        System.out.print("Masukkan angka:");
+        Scanner input = new Scanner(System.in);
+        
+        int angka = input.nextInt();
+        
+		int l = 1;
+		for(int i=1;i<=angka;i++){
+		for(int k=angka;k>i;k--){
+		System.out.print(" ");
+		}
+		for(int j=1;j<=l;j++){
+		System.out.print("+");
+		}
+		l+=2;
+		 
+		System.out.println("");
+		}
+		l -= 4;
+		 
+		for(int i=(angka-1);i>=1;i--){
+		for(int k=i;k<=(angka-1);k++){
+		System.out.print(" ");
+		}
+		for(int j=l;j>=1;j--){
+		System.out.print("+");
+		}
+		l-=2;
+		 
+		System.out.println("");
+		}
+ 
+	}
+
+}
